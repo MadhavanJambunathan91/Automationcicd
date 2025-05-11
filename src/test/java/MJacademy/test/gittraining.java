ecb2a64c736067a36b0cef1ff641777d8fa3c845
MadhavanJambunathan91 <madhavan915@gmail.com>
2025-05-11 11:56:38 +0530

    Initial files commit

---
+++ src/test/java/MJacademy/test/gittraining.java
@@ -0,0 +1,18 @@
+package MJacademy.test;
+
+public class gittraining {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] igitdemo = {22,34,5,8};
+		
+		for (int i=0;i<igitdemo.length;i++) {
+			
+			System.out.println( igitdemo[i] );
+			
+		}
+
+	}
+
+}
